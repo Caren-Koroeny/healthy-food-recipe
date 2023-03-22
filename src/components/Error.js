d60c5297ffc9d1075b4b134465ca27439b7d4eb1
Caren-Koroeny <carensiya5@gmail.com>
2023-03-22 10:46:29 +0300

    Used arrow function in Error file

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-function Error() {
-  return (
-    <div>
-      <h1>Kindly Navigate back to homepage, page does not exist</h1>
-    </div>
-  );
-}
+const Error = () => (
+  <div>
+    <h1>Kindly Navigate back to homepage, page does not exist</h1>
+  </div>
+);
 
 export default Error;
