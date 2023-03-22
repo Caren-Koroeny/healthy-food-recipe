fba0a30c1a707f0c88131b591d3d7db1addb66cc
Caren-Koroeny <carensiya5@gmail.com>
2023-03-22 10:44:45 +0300

    Fixed errors in category file and used arrow function

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,17 +1,15 @@
 import filters from './filterArray';
 
-function CategoryFilter({ handleFilterChange }) {
-  return (
-    <div>
-      <select className="select" onChange={handleFilterChange} name="filter">
-        {filters.map((filter) => (
-          <option name="category" value={filter} key={filter}>
-            {filter}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-}
+const CategoryFilter = ({ handleFilterChange }) => (
+  <div>
+    <select className="select" onChange={handleFilterChange} name="filter">
+      {filters.map((filter) => (
+        <option name="category" value={filter} key={filter}>
+          {filter}
+        </option>
+      ))}
+    </select>
+  </div>
+);
 
 export default CategoryFilter;
