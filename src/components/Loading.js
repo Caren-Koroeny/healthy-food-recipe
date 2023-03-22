4196c1495630b4900428322e167ede2a2dd21e31
Caren-Koroeny <carensiya5@gmail.com>
2023-03-22 08:53:08 +0300

    Add meal section in the components to display it on the UI

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Meal.css';
+
+function Loading() {
+  return (
+    <div className="loading">
+      <h1>loading...</h1>
+    </div>
+  );
+}
+
+export default Loading;
---
+++ src/components/Meal.js
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Meal.css';
+
+function Meal({ id, name, image }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        rootMargin: '0px',
+        threshold: 0.5,
+      },
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return (
+    <section className="meal-container">
+      <div
+        className={`meal-card ${isVisible ? 'fade-in is-visible' : 'fade-in'}`}
+        data-testid="meal-card"
+        key={id}
+        ref={ref}
+      >
+        <Link className="cards " to={`/meal/${id}`}>
+          <div>
+            <img className="image" src={image} alt={name} />
+          </div>
+          <div>
+            <h3 className="title">{name}</h3>
+          </div>
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+export default Meal;
