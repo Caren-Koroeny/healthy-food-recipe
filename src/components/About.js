8ae488491c7cf1015ee3a917f966d77e5473b954
Caren-Koroeny <carensiya5@gmail.com>
2023-03-22 10:35:17 +0300

    use Arrow function in about file

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,20 @@
 import React from 'react';
 
-function About() {
-  return (
-    <div className="about-us">
-      <h1 className="about">About Us</h1>
-      <p>
-        Yummy Kitchen- Italia Pizza.
-        “A recipe has no soul. You, as the cook, must bring soul to the recipe.”
-
-      </p>
-      <p>
-        “Those who will gratify their appetite,
-        and then suffer because of their intemperance,
-        and take drugs to relieve them, may be assured
-        that God will not interpose to save health and life which is so recklessly periled. 
-        The cause has produced the effect.”
-      </p>
-    </div>
-  );
-}
+const About = () => (
+  <div className="about-us">
+    <h1 className="about">About Us</h1>
+    <p>
+      Yummy Kitchen- Italia Pizza.
+      “A recipe has no soul. You, as the cook, must bring soul to the recipe.”
+    </p>
+    <p>
+      “Those who will gratify their appetite,
+      and then suffer because of their intemperance,
+      and take drugs to relieve them, may be assured
+      that God will not interpose to save health and life which is so recklessly periled.
+      The cause has produced the effect.”
+    </p>
+  </div>
+);
 
 export default About;
