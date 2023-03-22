848b8136d54061026404fbddb350dc7f6ab01109
Caren-Koroeny <carensiya5@gmail.com>
2023-03-22 09:03:48 +0300

    Added Error file

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import MealList from '../containers/MealList';
+import MealDetails from '../containers/MealDetails';
+import Error from './Error';
+import About from './About';
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<MealList />} />
+          <Route path="/Meal/:id" element={<MealDetails />} />
+          <Route exact path="About" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Error() {
+  return (
+    <div>
+      <h1>Kindly Navigate bck to homepage, page does not exist</h1>
+    </div>
+  );
+}
+
+export default Error;
