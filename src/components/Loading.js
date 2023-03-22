07a3f6103bdf21858a1b9c5d1fbd87802349f751
Caren-Koroeny <carensiya5@gmail.com>
2023-03-22 10:48:04 +0300

    removed unsed line of codes in loading file

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import '../styles/Meal.css';
 
-function Loading() {
-  return (
-    <div className="loading">
-      <h1>loading...</h1>
-    </div>
-  );
-}
+const Loading = () => (
+  <div className="loading">
+    <h1>loading...</h1>
+  </div>
+);
 
 export default Loading;
