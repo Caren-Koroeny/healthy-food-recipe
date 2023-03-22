dc0497f23655e443ca82ef29df908b5b36174551
Caren-Koroeny <carensiya5@gmail.com>
2023-03-22 10:37:37 +0300

    Convert the function to arrow function in App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,18 @@ import MealDetails from '../containers/MealDetails';
 import Error from './Error';
 import About from './About';
 
-function App() {
-  return (
-    <div className="app">
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<MealList />} />
-          <Route path="/Meal/:id" element={<MealDetails />} />
-          <Route exact path="About" element={<About />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-}
+const App = () => (
+  <div className="app">
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<MealList />} />
+        <Route path="/Meal/:id" element={<MealDetails />} />
+        <Route exact path="About" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </div>
+);
 
 export default App;
