4a375bd1c608332510ddbff971d37a142090fbb4
Caren-Koroeny <carensiya5@gmail.com>
2023-03-23 16:18:33 +0300

    Updated back link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,19 @@
 import { FaArrowAltCircleLeft, FaMicrophone } from 'react-icons/fa';
 import { SlSettings } from 'react-icons/sl';
 import { Link } from 'react-router-dom';
-import MealList from '../containers/MealList';
-import Meal from './Meal';
+// import MealList from '../containers/MealList';
 import '../styles/NavBar.css';
 import '../styles/MealList.css';
 
 const NavBar = () => (
   <nav data-testid="nav" className="nav">
-    <Link className="back" to="/" onClick={() => Meal()}>
+    <Link className="back" to="/">
       {' '}
       <FaArrowAltCircleLeft className="icons" />
       {' '}
     </Link>
     <div className="home-kitchen">
-      <Link className="heading" to="/" onClick={() => MealList()}>
+      <Link className="heading" to="/">
         <h2>Home</h2>
       </Link>
     </div>
