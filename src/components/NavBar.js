4f86f3b4a3c56a92b9096b474f2d9d1cc1bb405b
Caren-Koroeny <carensiya5@gmail.com>
2023-03-25 20:48:42 +0300

    Add useNavigate

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,36 @@
 import { FaArrowAltCircleLeft, FaMicrophone } from 'react-icons/fa';
 import { SlSettings } from 'react-icons/sl';
-import { Link } from 'react-router-dom';
-import MealList from '../containers/MealList';
+import { Link, useNavigate } from 'react-router-dom';
+// import MealList from '../containers/MealList';
 import '../styles/NavBar.css';
 import '../styles/MealList.css';
 
-const NavBar = () => (
-  <nav data-testid="nav" className="nav">
-    <Link className="back" to="/" onClick={() => MealList()}>
-      {' '}
-      <FaArrowAltCircleLeft className="icons" />
-      {' '}
-    </Link>
-    <div className="home-kitchen">
-      <Link className="heading" to="/">
-        <h2>Home</h2>
+const NavBar = () => {
+  const history = useNavigate();
+  const handleBackClick = () => {
+    history(-1);
+  };
+
+  return (
+    <nav data-testid="nav" className="nav">
+      <Link className="back" to="/" onClick={handleBackClick}>
+        {' '}
+        <FaArrowAltCircleLeft className="icons" />
+        {' '}
       </Link>
-    </div>
-    <div>
-      <Link className="icons1" to="/">
-        <FaMicrophone className="icons1" />
-        <SlSettings className="icons1" />
-      </Link>
-    </div>
-  </nav>
-);
+      <div className="home-kitchen">
+        <Link className="heading" to="/">
+          <h2>Home</h2>
+        </Link>
+      </div>
+      <div>
+        <Link className="icons1" to="/">
+          <FaMicrophone className="icons1" />
+          <SlSettings className="icons1" />
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
